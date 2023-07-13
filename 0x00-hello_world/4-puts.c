05d198c8b002f48c77f40ef23ed03ae877d2c863
ndiramiye2 <onlythenotes@gmail.com>
2023-07-13 11:27:53 -0700

    ~main fction in single file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,9 @@
-#include<stdio.h>
-
-/**
- *main - print  "programing is like building a multilingual
- * puzzle, followed by a newline.
+#include<stdio.h>/**
+ * main - print "Programming is like building a multilingual
+ * puzzle,  followed by a newline.
  * Return: Always 0.
- */
-int main(void)
+ */int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 }
