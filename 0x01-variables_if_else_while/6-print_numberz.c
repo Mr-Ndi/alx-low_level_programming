cac3a59e439de9e3085608c195b6c6bb6ac47250
ndiramiye2 <onlythenotes@gmail.com>
2023-07-14 15:02:06 -0700

    numberz but by putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - main function khabibulah wake up
+ * Return: always 0.
+ */
+int main(void)
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
