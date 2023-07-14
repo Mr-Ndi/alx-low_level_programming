3d4a15b94846f4d2f81801612842d6ae489580f5
ndiramiye2 <onlythenotes@gmail.com>
2023-07-14 12:40:40 -0700

    the klast digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,17 +12,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	k = n % 10;
-	if(k > 5)
+	if (k > 5)
 	{
-	printf("the last digit of %d  is %d and is greater than 5",n,k);
+	printf("the last digit of %d  is %d and is greater than 5", n, k);
 	}
 	else if (k == 0)
 	{
-	printf("the last digit of %d is %d ",n,k);
+	printf("the last digit of %d is %d ", n, k);
 	}
 	else
 	{
-	printf("the last digit of %d is %d and is less than 6 and not 0");
+	printf("the last digit of %d is %d and is less than 6 and not 0", n, k);
 	}
 	return (0);
 }
