b34bcbad0578dba5244ea8ad57c4bf9e60da19e7
ndiramiye2 <onlythenotes@gmail.com>
2023-07-14 15:57:48 -0700

    hexadecimal numbers

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main cpn wabibonye ko comment doesn't matter
+ * Return: always 0.
+ */
+int main(void)
+{
+int p;
+int k;
+for (p = 48; p <= 57; p++)
+{
+putchar(p);
+}
+for (k =97; k <= 102; k++)
+{
+putchar(k);
+}
+putchar('\n');
+return (0);
+}
