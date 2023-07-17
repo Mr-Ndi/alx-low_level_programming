6a45f117d7b9a496c6e5eba259d51433dbcdc3c5
ndiramiye2 <onlythenotes@gmail.com>
2023-07-17 14:02:07 -0700

    final one

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+int main(void)
+{
+int q, w;
+
+for (q = 0; q <= 99; q++)
+{
+for (w = q + 1; w <= 99; w++)
+{
+putchar(q / 10 + '0');
+putchar(q % 10 + '0');
+putchar(' ');
+putchar(w / 10 + '0');
+putchar(q % 10 + '0');
+if (!(q == 98 && w == 99))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
