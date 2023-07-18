a73ad7ac0330577f653bb4893d6fc900306e9c45
ndiramiye2 <onlythenotes@gmail.com>
2023-07-18 00:31:43 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entre point.
+ * Description:displaying putchar.
+ * Return: always 0 indicating successful execution.
+ */
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+this is a read me file iside the 0x02-functions nested loops that focus on
+0x02. C - Functions, nested loops
