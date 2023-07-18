87ddd7878028fa61c5f198699280e41495941363
ndiramiye2 <onlythenotes@gmail.com>
2023-07-18 07:21:23 -0700

    kjh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
+void prtint_alphabet(void)
+{
+char l;
+
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
+}
