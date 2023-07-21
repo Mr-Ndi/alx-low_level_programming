c00ab6b2d72b06a152ffa540f29c6e7fc663a1e5
ndiramiye2 <onlythenotes@gmail.com>
2023-07-21 14:38:54 -0700

    last task

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,46 +1,20 @@
 #include "main.h"
 
 /**
- * print_number -  checks for checks for a digit (0 through 9).
- * @n: n -  Variable
- * Return: Always 0.
+ * print_number -  prints an integer using _putchar
+ * @n: The integer to be printed
  */
 void print_number(int n)
 {
-nsigned int z;
-int m, b;
-
-b = 10;
-
-if (n < 10 && n >= 0)
-{
-_putchar (n + '0');
-}
-else if (n > -10 && n < 0)
-{
-n = n - 2 * n;
-_putchar('-');
-_putchar (n + '0');
-}
-
-else
-{
 if (n < 0)
 {
-n = n * -1;
-_putchar ('-');
+_putchar('-');
+n = -n;
 }
-z = n;
-while (z / b > 9)
-{
-b = b * 10;
+
+if (n / 10 != 0)
+print_number(n / 10);
+
+_putchar((n % 10) + '0');
 }
-while (b > 0)
-{
-m = z / b;
-z = z % b;
-_putchar (m + '0');
-b = b / 10;
-}
-}
-}
+
