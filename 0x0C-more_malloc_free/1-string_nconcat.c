57ff7d8ea4781cfbab12dce3e26294ec5fcbf6a2
ndiramiye2 <onlythenotes@gmail.com>
2023-08-10 09:08:04 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 # include "main.h"
 # include <stdlib.h>
-# include <stdio.h>
 
 /**
  * string_nconcat -  a function that concatenates two strings
---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include "main.h"
+
+/**
+ * _realloc - a function that reallocates a memory block.
+ * @ptr:
+ * @old_size:
+ * @new_size:
+ *
+ * Return:
