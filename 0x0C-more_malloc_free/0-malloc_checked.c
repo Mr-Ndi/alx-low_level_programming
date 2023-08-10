47ab6977939df33f341acf82ba60a3b9fa04db37
ndiramiye2 <onlythenotes@gmail.com>
2023-08-10 06:46:48 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+# include "main.h"
+# include <stdlib.h>
+# include <stdio.h>
+
+/**
+ * malloc_checked.c - a function that allocates memory using malloc.
+ * @b: the size to allocate
+ *
+ * Return:Null if b is null otherwise  allocates memory using malloc
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This is the file that includes tasks for project ==> 0x0C. C - More malloc, free
