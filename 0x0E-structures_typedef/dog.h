06615cf089880f61e621dc78899cd4af25a589a7
ndiramiye2 <onlythenotes@gmail.com>
2023-08-14 09:14:28 -0700

    poopy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,20 @@
 # ifndef DOG_H
 # define DOG_H
 
+/**
+ * struct dog - basic dog information
+ * @name: name of the dog
+ * @age: age of yhe dog
+ * @owner: the dog's owner
+ *
+ * Description : continues
+ */
+
  struct dog{
 	char *name;
 	float age;
 	char *owner;
 };
+
 
 # endif
