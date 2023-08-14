a23d4e5038368a78792a2514ff75e7ef2af8c19c
ndiramiye2 <onlythenotes@gmail.com>
2023-08-14 07:14:19 -0700

    2 tasks 2 and 4

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 # include <stdio.h>
-int main()
+
+`/**
+ * main - a function to prints the name of the file it
+ *  was compiled from, followed by a new line
+ */
+
+int main(viod)
 {
 printf("%s\n", __FILE__);
 return (0);
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,6 +1,6 @@
 # ifndef SUM_OF_TWO_NUMBER
 # define SUM_OF_TWO_NUMBER
 
-# define SUM(x, y) (x) + (y)
+# define SUM(x, y) ((x) + (y))
 
 # endif
