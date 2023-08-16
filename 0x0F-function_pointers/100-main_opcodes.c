f842cf959f367320f62ee8ff732b440f9c4f316b
ndiramiye2 <onlythenotes@gmail.com>
2023-08-16 12:48:53 -0700

    not sure

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,42 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- *print_opcodes - function to print ouw opcodes of main
- * Return: nothing
+ * print_opcodes - function to print opcodes of main
+ * @main_ptr: pointer to the main function
  */
-void print_opcodes(void)
-{
-int i;
+void print_opcodes(unsigned char *main_ptr) {
+unsigned char *print_opcodes_ptr = (unsigned char *) &print_opcodes;
 
-char *main_ptr = (char *) &print_opcodes;
-
-for (i = 0; i < sizeof(print_opcodes); i++)
-{
-printf("%02x", main_ptr[i] & 0xff);
-
+for (; print_opcodes_ptr < main_ptr; print_opcodes_ptr++) {
+printf("%02x ", *print_opcodes_ptr);
 }
+
 printf("\n");
 }
+
 /**
  * main - main entrance
  * @argc: number of arguments
- * @argv: arrys of argments
+ * @argv: array of arguments
  * Return: always 0
  */
-int main(int argc, char *argv[])
-{
-if (argc != 2)
-{
+int main(int argc, char *argv[]) {
+if (argc != 2) {
 printf("Error\n");
-return (1);
+return 1;
 }
+
 int num_bytes = atoi(argv[1]);
 
-if (num_bytes < 0)
-{
+if (num_bytes < 0) {
 printf("Error\n");
-return (2);
+return 2;
 }
-print_opcodes();
-return (0);
+
+unsigned char *main_ptr = (unsigned char *) &main;
+print_opcodes(main_ptr);
+
+return 0;
 }
