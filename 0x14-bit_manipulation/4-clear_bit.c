a9ab2f14181ba8db63f00e7ea91015761ddcc697
ndiramiye2 <onlythenotes@gmail.com>
2023-09-01 06:54:10 -0700

    new version of 4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,6 +10,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int ) * 8)
 		return (-1);
-	*n = *n & ~(1UL << index);
+	*n = *n & ~(1 << index);
 	return (1);
 }
