e6bdb39ba7f959f6ad82da47b9ded48f700b4c6a
ndiramiye2 <onlythenotes@gmail.com>
2023-09-04 15:37:19 -0700

    task 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,23 +11,23 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int a, b;
-	mode_t mode = S_IRUSR | S_IWUSR;
+	int a, b, length = 0;
 
 	if (filename == NULL)
-		return (0);
-	a = open(filename, O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (a == -1)
 		return (-1);
 	if (text_content != NULL)
 	{
-		b = write(a, text_content, strlen(text_content));
-		if (b == -1)
-		{
-			close(a);
-			return (-1);
-		}
+		while (text_content[length])
+			length++;
+
 	}
+	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IWUSR | S_IRUSR);
+	b = write(a, text_content, length);
+
+	if (a == -1 || b == -1)
+		return (-1);
+
 	close(a);
-	return (-1);
+
+	return (1);
 }
