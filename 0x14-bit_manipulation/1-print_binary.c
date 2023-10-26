3a30da4c2fce16f7a0cc79ad6a3b2c3be39d176d
ndiramiye2 <onlythenotes@gmail.com>
2023-10-26 11:30:08 -0700

    task2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - Prints the binary representation of a decimal number
+ * @num: The decimal number to print in binary
+ */
+void print_binary(unsigned long int num)
+{
+	int bit_position, count = 0;
+	unsigned long int current_bit;
+
+	for (bit_position = 63; bit_position >= 0; bit_position--)
+	{
+		current_bit = num >> bit_position;
+
+		if (current_bit & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+		else if (count)
+			_putchar('0');
+	}
+
+	if (!count)
+		_putchar('0');
+
+	_putchar('\n');
+}
