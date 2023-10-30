8b712f61a90c1331122df5c78f2f1b17dbc826fb
ndiramiye2 <onlythenotes@gmail.com>
2023-10-30 23:29:31 +0200

    octopus

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -11,39 +12,40 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buf;
-	ssize_t n_read, n_written;
+    int fd;
+    char *buf;
+    ssize_t n_read, n_written;
 
-	if (filename == NULL)
-		return (0);
+    if (filename == NULL)
+        return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    buf = malloc(sizeof(char) * letters);
+    if (buf == NULL)
+    {
+        close(fd);
+        return (0);
+    }
 
-	n_read = read(fd, buf, letters);
-	if (n_read == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
+    n_read = read(fd, buf, letters);
+    if (n_read == -1)
+    {
+        free(buf);
+        close(fd);
+        return (0);
+    }
 
-	n_written = write(STDOUT_FILENO, buf, n_read);
+    n_written = write(1, buf, n_read);
 
-	free(buf);
-	close(fd);
+    free(buf);
+    close(fd);
 
-	if (n_written == n_read)
-		return n_written;
-	else
-		return (0);
+    if (n_written == n_read)
+        return n_written;
+    else
+        return (0);
 }
+
