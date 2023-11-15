03951b0116e8dd23597254c893ab46b58857fc6c
ndiramiye2 <onlythenotes@gmail.com>
2023-11-15 18:42:48 +0200

    last modification after changing github username

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,31 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-    int fd, res_write, len;
+	ssize_t fd;
+	int i = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 
-    if (text_content != NULL)
-    {
-        for (len = 0; text_content[len] != '\0'; len++)
-            ;
+	if (fd == -1)
+		return (-1);
 
-        res_write = write(fd, text_content, len);
-        if (res_write == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (!text_content)
+		return (1);
 
-    close(fd);
-    return (1);
+	while (text_content[i])
+	{
+		if (write(fd, &text_content[i], 1) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		i++;
+	}
+
+	close(fd);
+
+	return (1);
 }
