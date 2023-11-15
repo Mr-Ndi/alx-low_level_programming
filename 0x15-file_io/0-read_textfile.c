edc1f1e681e41d2a42367702c89398a3a3406d24
ndiramiye2 <onlythenotes@gmail.com>
2023-11-15 18:38:44 +0200

    last modification after changing github username

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,40 +12,38 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    char *buf;
-    ssize_t n_read, n_written;
+	ssize_t fd;
+	ssize_t read_bytes;
+	char *buffer;
+	ssize_t written_bytes;
 
-    if (filename == NULL)
-        return (0);
+	buffer = malloc(sizeof(char) * letters);
 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
+	if (!filename || !buffer)
+		return (0);
 
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-    {
-        close(fd);
-        return (0);
-    }
+	fd = open(filename, O_RDONLY);
 
-    n_read = read(fd, buf, letters);
-    if (n_read == -1)
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
+	if (fd == -1)
+		return (0);
 
-    n_written = write(1, buf, n_read);
+	read_bytes = read(fd, buffer, letters);
 
-    free(buf);
-    close(fd);
+	if (read_bytes == -1)
+	{
+		close(fd);
+		return (0);
+	}
 
-    if (n_written == n_read)
-        return n_written;
-    else
-        return (0);
+	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+
+	if (written_bytes == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+
+	return (written_bytes);
 }
-
