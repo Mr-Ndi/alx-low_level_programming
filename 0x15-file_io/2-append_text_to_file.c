28cdbd6dbd98689d57fd84fd1f14dd4305d29d70
ndiramiye2 <onlythenotes@gmail.com>
2023-11-15 18:57:33 +0200

    last modification after changing github username

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,30 +7,35 @@
  *
  * Return: 1 on success, -1 on failure
  */
+
+
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, res_write, len;
+	ssize_t fd;
+	int i = 0;
 
-    if (filename == NULL)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    fd = open(filename, O_WRONLY | O_APPEND);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    if (text_content != NULL)
-    {
-        for (len = 0; text_content[len] != '\0'; len++)
-            ;
+	if (fd == -1)
+		return (-1);
 
-        res_write = write(fd, text_content, len);
-        if (res_write == -1)
-        {
-            close(fd);
-            return (-1);
-        }
-    }
+	if (!text_content)
+		return (1);
 
-    close(fd);
-    return (1);
+	while (text_content[i])
+	{
+		if (write(fd, &text_content[i], 1) == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+		i++;
+	}
+
+	close(fd);
+
+	return (1);
 }
