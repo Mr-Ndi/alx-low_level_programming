96f3b6a170ab93e3408ecb260acf1fe230e7626b
ndiramiye2 <onlythenotes@gmail.com>
2024-04-10 10:49:58 +0200

    Task_0_linearsearh_bwa3

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,15 +12,15 @@ int linear_search(int *array, size_t size, int value)
 	size_t i = 0;
 
 	if (array == NULL)
-			return (-1);
+		return (-1);
 	for (; i < size; i++)
 	{
-			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-			if (value == array[i])
-			{
-					break;
-					return (i);
-			}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (value == array[i])
+		{
+			break;
+			return (i);
+		}
 	}
 	return (-1);
 }
